d42b3505d5359712b529cd83f5a07ee4a2812efe
fervent-Coder <158794085+fervent-Coder@users.noreply.github.com>
2025-05-27 07:58:37 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Github, Mail } from 'lucide-react';
 import './App.css';
 
 const projects = [
+  { title: "Insulin Machine Simulation", description: "A full-stack University group project that simulated Tandem t:slim X2 Insulin Pump is a system designed to deliver precise insulin dosages for effective diabetes management. The goal of this project is to develop an interactive software simulation of the t:slim X2 pump in Qt/C++.", link: "https://github.com/team-62-hq/Insulin-Pump-Simulation" },
   { title: "store", description: "A full-stack project utilizing MongolDB, react ,expresjs and node js to make a store that adds products with their name price and a url picture .", link: "https://github.com/fervent-Coder/Store" },
   { title: "portofolio", description: "Website i made to showcase my projects using react , html and css.", link: "https://github.com/fervent-Coder/Personal-website" },
 ];
